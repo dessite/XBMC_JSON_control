03e1b61c7a67a40502860e5eaa767d239c87178c
Dessite <fubu666@gmail.com>
2015-01-21 20:38:21 +0100

    work in progress

--- xbmc_json_control.py
+++ xbmc_json_control.py
@@ -1,10 +1,14 @@
-__author__ = 'fuf'
-import requests
+__author__ = 'Dessite'
+__url__ = 'https://github.com/dessite/XBMC_JSON_control'
+__email_ = 'fubu666@gmail.com'
+
 import json
+
+import requests
+
 
 left = {"jsonrpc": "2.0", "method": "Input.left", "id": "1"}
 url = "http://192.168.1.105/jsonrpc"
-
 
 while True:
     key = input()
@@ -14,4 +18,4 @@ while True:
     elif key == 'b':
         break
     else:
-        print("cos innego")
+        print("something else")
