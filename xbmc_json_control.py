48c4c45531d88e37e8b9c649cee35464e46583a8
Dessite <fubu666@gmail.com>
2015-01-21 21:16:31 +0100

    Simple control by entering direction letters, no going back

--- xbmc_json_control.py
+++ xbmc_json_control.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 __author__ = 'Dessite'
 __url__ = 'https://github.com/dessite/XBMC_JSON_control'
 __email_ = 'fubu666@gmail.com'
@@ -6,16 +8,52 @@ import json
 
 import requests
 
+# Directions
+_up = {"jsonrpc": "2.0", "method": "Input.up", "id": "1"}
+_down = {"jsonrpc": "2.0", "method": "Input.down", "id": "1"}
+_left = {"jsonrpc": "2.0", "method": "Input.left", "id": "1"}
+_right = {"jsonrpc": "2.0", "method": "Input.right", "id": "1"}
 
-left = {"jsonrpc": "2.0", "method": "Input.left", "id": "1"}
-url = "http://192.168.1.105/jsonrpc"
+# XBMC JSON-RPC API URL
+_url = "http://192.168.1.105/jsonrpc"
+
+print("Welcome to XBMC_JSON_CONTROL")
+print("Enter u for up, d for down, l for left, r for right")
+print("Enter s to select")
+print("Enter q to quit")
 
 while True:
-    key = input()
-    if key == 'l':
-        request = requests.post(url, data=json.dumps(left))
-        print(request.json())
-    elif key == 'b':
+    try:
+        key = input()
+    except:
+        print("Something went wrong, exiting")
+        break
+
+    if key == 'u':
+        try:
+            request = requests.post(_url, data=json.dumps(_up))
+        except:
+            print("Somethings wrong, is Your XBMC running?")
+    elif key == 'd':
+        try:
+            request = requests.post(_url, data=json.dumps(_down))
+        except:
+            print("Somethings wrong, is Your XBMC running?")
+    elif key == 'l':
+        try:
+            request = requests.post(_url, data=json.dumps(_left))
+        except:
+            print("Somethings wrong, is Your XBMC running?")
+    elif key == 'r':
+        try:
+            request = requests.post(_url, data=json.dumps(_right))
+        except:
+            print("Somethings wrong, is Your XBMC running?")
+    elif key == 'q':
+        print("Goodbye")
         break
     else:
-        print("something else")
+        print("You've entered something else")
+        print("Enter u for up, d for down, l for left, r for right")
+        print("Enter s to select")
+        print("Enter q to quit")
