dde894aee0345a2f5411e1ad7d3eca69c94fc3df
Dessite <fubu666@gmail.com>
2015-01-25 00:00:24 +0100

    Added discovery of XBMC instances Added back button action Moved some things to functions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # XBMC_JSON_control
 Simple app to control XBMC via JSON requests
+The goal is to run this app on Raspberry PI with Raspbmc and control XBMC via push-buttons connected to GPIO
+Currently it reads input from stdin however I plan to add support for Raspberry PI GPIO in the future, when I'll have some time to buy needed components and build steering board.
+
+The app currently discovers XBMC instances with Zeroconf enabled, and then controls XBMC via simple JSON-RPC requests provided by XBMC JSON-RPC API
--- xbmc_json_control.py
+++ xbmc_json_control.py
@@ -5,61 +5,108 @@ __url__ = 'https://github.com/dessite/XBMC_JSON_control'
 __email_ = 'fubu666@gmail.com'
 
 import json
+import time
+import socket
 
 import requests
+from zeroconf import *
 
-# Directions
-_up = {"jsonrpc": "2.0", "method": "Input.up", "id": "1"}
-_down = {"jsonrpc": "2.0", "method": "Input.down", "id": "1"}
-_left = {"jsonrpc": "2.0", "method": "Input.left", "id": "1"}
-_right = {"jsonrpc": "2.0", "method": "Input.right", "id": "1"}
-_select = {"jsonrpc": "2.0", "method": "Input.select", "id": "1"}
 
-# XBMC JSON-RPC API URL
-_url = "http://192.168.1.105/jsonrpc"
 
-print("Welcome to XBMC_JSON_CONTROL")
-print("Enter u for up, d for down, l for left, r for right")
-print("Enter s to select")
-print("Enter q to quit")
 
-while True:
-    try:
-        key = input()
-    except:
-        print("Something went wrong, exiting")
-        break
+# listener class for XBMC
+class XBMClistener(object):
+    info = []
 
-    if key == 'u':
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
+
+    def add_service(self, zeroconf, service_type, name):
+        self.info.append(zeroconf.get_service_info(service_type, name))
+
+
+# search for running XBMC instances
+def scan_for_xbmc():
+    zeroconf = Zeroconf()
+    xbmc_listener = XBMClistener()
+    ServiceBrowser(zeroconf, "_xbmc-jsonrpc._tcp.local.", xbmc_listener)
+    time.sleep(6)
+    zeroconf.close()
+    return xbmc_listener.info
+
+
+# control XBMC instance
+def control_xbmc(_ip):
+    # XBMC JSON-RPC API URL
+    _url = "http://" + _ip + "/jsonrpc"
+
+    # Directions
+    _up = {"jsonrpc": "2.0", "method": "Input.up", "id": "1"}
+    _down = {"jsonrpc": "2.0", "method": "Input.down", "id": "1"}
+    _left = {"jsonrpc": "2.0", "method": "Input.left", "id": "1"}
+    _right = {"jsonrpc": "2.0", "method": "Input.right", "id": "1"}
+    _select = {"jsonrpc": "2.0", "method": "Input.select", "id": "1"}
+    _back = {"jsonrpc": "2.0", "method": "Input.back", "id": "1"}
+
+    while True:
         try:
-            request = requests.post(_url, data=json.dumps(_up))
+            key = input()
         except:
-            print("Somethings wrong, is Your XBMC running?")
-    elif key == 'd':
-        try:
-            request = requests.post(_url, data=json.dumps(_down))
-        except:
-            print("Somethings wrong, is Your XBMC running?")
-    elif key == 'l':
-        try:
-            request = requests.post(_url, data=json.dumps(_left))
-        except:
-            print("Somethings wrong, is Your XBMC running?")
-    elif key == 'r':
-        try:
-            request = requests.post(_url, data=json.dumps(_right))
-        except:
-            print("Somethings wrong, is Your XBMC running?")
-    elif key == 's':
-        try:
-            request = requests.post(_url, data=json.dumps(_select))
-        except:
-            print("Somethings wrong, is Your XBMC running?")
-    elif key == 'q':
-        print("Goodbye")
-        break
-    else:
-        print("You've entered something else")
-        print("Enter u for up, d for down, l for left, r for right")
-        print("Enter s to select")
-        print("Enter q to quit")
+            print("Something went wrong, exiting")
+            break
+
+        if key == 'u':
+            try:
+                requests.post(_url, data=json.dumps(_up))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 'd':
+            try:
+                requests.post(_url, data=json.dumps(_down))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 'l':
+            try:
+                requests.post(_url, data=json.dumps(_left))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 'r':
+            try:
+                requests.post(_url, data=json.dumps(_right))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 's':
+            try:
+                requests.post(_url, data=json.dumps(_select))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 'b':
+            try:
+                requests.post(_url, data=json.dumps(_back))
+            except:
+                print("Somethings wrong, is Your XBMC running?")
+        elif key == 'q':
+            print("Goodbye")
+            break
+        else:
+            print("You've entered something else")
+            print("Enter u for up, d for down, l for left, r for right")
+            print("Enter s to select")
+            print("Enter q to quit")
+
+
+if __name__ == "__main__":
+    print("Welcome to XBMC_JSON_CONTROL")
+    print("Searching for XBMC instances, please wait")
+
+    instances = scan_for_xbmc()
+
+    # TODO selection from multiple instances
+    print("Found " + instances[0].name + " at IP: " + socket.inet_ntoa(instances[0].address))
+
+    print("Enter u for up, d for down, l for left, r for right")
+    print("Enter s to select")
+    print("Enter q to quit")
+
+    control_xbmc(socket.inet_ntoa(instances[0].address))
+
